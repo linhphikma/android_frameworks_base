d8e8c479ca9a0d64e0e57c7420a3c5dd38c47bb7
Apr20th <1131141128@qq.com>
2015-11-20 19:53:53 +0800

    test

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2487,7 +2487,23 @@ public final class Settings {
 		public static final String TRANSLUCENT_NOTIFICATIONS_PREFERENCE_KEY = "hook_system_ui_translucent_notifications_pref";
 
         /**
-         * Scaling factor for fonts, float.
+         * Whether to enable Swipe Back Layout Gesture
+         * Default: 0
+         *
+         * @hide
+         */
+        public static final String SWIPE_BACK_GESTURE_ENABLED = "swipe_back_gesture_enabled";
+
+        /**
+         * Swipe edge of the swipe back layout gesture
+         *
+         * @hide
+         */
+        public static final String SWIPE_BACK_GESTURE_EDGE = "swipe_back_gesture_edge";
+
+        /**
+         * Chinese date view
+         * @hide
          */
         public static final String FONT_SCALE = "font_scale";
 
