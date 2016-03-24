2de67b9c3e93c2cf17827f2f26916a3d6f6ede80
Linh Phi <linhphi9x94@gmail.com>
2016-03-24 18:11:53 +0700

    Revert "Base: Clean Up StatusBar Colors[1/2]"
    This reverts commit 50b22f43cae0005b25ecf0323e2b451a9fbf23e8.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -858,16 +858,25 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                    updatebatterycolor(); 
 	   } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.STATUSBAR_COLOR_SWITCH))) {
-              		updateNetworkSignalColor();
-	      		updateNoSimColor();
-	      		updateAirplaneModeColor();
-	      		updateStatusIconsColor();	
-	      		updateNotificationIconsColor();
-	      		updateNetworkIconColors();
-		 	updateRowStates();
-               		updateSpeedbump();
-                	updateClearAll();
-                	updateEmptyShadeView();
+                recreateStatusBar();
+                updateRowStates();
+                updateSpeedbump();
+                updateClearAll();
+                updateEmptyShadeView();
+	   }  else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.BATTERY_ICON_COLOR))) {
+                 recreateStatusBar();
+                updateRowStates();
+                updateSpeedbump();
+                updateClearAll();
+                updateEmptyShadeView();
+	   }  else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.BATTERY_TEXT_COLOR))) {
+                recreateStatusBar();
+                updateRowStates();
+                updateSpeedbump();
+                updateClearAll();
+                updateEmptyShadeView();
 	   } 
             update();
         }
@@ -3002,11 +3011,17 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     
     
     private void updateNetworkIconColors() {
+	mColorSwitch =  Settings.System.getInt(mContext.getContentResolver(),
+				 Settings.System.STATUSBAR_COLOR_SWITCH, 0) == 1;
         if (mIconController != null) {
+	    if(mColorSwitch) {
             mIconController.updateNetworkIconColors();
+	   }
         }
         if (mKeyguardStatusBar != null) {
+	     if(mColorSwitch) {
             mKeyguardStatusBar.updateNetworkIconColors();
+	    }
         }
     }
     
@@ -3022,36 +3037,57 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     private void updateNetworkSignalColor() {
+	mColorSwitch =  Settings.System.getInt(mContext.getContentResolver(),
+				 Settings.System.STATUSBAR_COLOR_SWITCH, 0) == 1;
         if (mIconController != null) {
+	    if(mColorSwitch) {
             mIconController.updateNetworkSignalColor();
+		}
         }
         if (mKeyguardStatusBar != null) {
+	    if(mColorSwitch) {
             mKeyguardStatusBar.updateNetworkIconColors();
+	    }
         }
     }
 
     private void updateNoSimColor() {
+	mColorSwitch =  Settings.System.getInt(mContext.getContentResolver(),
+				 Settings.System.STATUSBAR_COLOR_SWITCH, 0) == 1;
         if (mIconController != null) {
+	     if(mColorSwitch) {
             mIconController.updateNoSimColor();
+	   }
         }
         if (mKeyguardStatusBar != null) {
+	     if(mColorSwitch) {
             mKeyguardStatusBar.updateNoSimColor();
+	   }
         }
     }
 
     private void updateAirplaneModeColor() {
+	mColorSwitch =  Settings.System.getInt(mContext.getContentResolver(),
+				 Settings.System.STATUSBAR_COLOR_SWITCH, 0) == 1;
         if (mIconController != null) {
+	    if(mColorSwitch) {
             mIconController.updateAirplaneModeColor();
+	   }
         }
         if (mKeyguardStatusBar != null) {
+	    if(mColorSwitch) {
             mKeyguardStatusBar.updateAirplaneModeColor();
+	    }
         }
     }
 
     private void updateStatusIconsColor() {
-
+	mColorSwitch =  Settings.System.getInt(mContext.getContentResolver(),
+				 Settings.System.STATUSBAR_COLOR_SWITCH, 0) == 1;
         if (mIconController != null) {
+	    if(mColorSwitch) {
             mIconController.updateStatusIconsColor();
+	    }
         }
     }
 
@@ -3059,7 +3095,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 	mColorSwitch =  Settings.System.getInt(mContext.getContentResolver(),
 				 Settings.System.STATUSBAR_COLOR_SWITCH, 0) == 1;
         if (mIconController != null) {
+	    if(mColorSwitch) {
             mIconController.updateNotificationIconsColor();
+	    }
         }
     }
 
