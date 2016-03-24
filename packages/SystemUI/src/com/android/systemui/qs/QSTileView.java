e2a9ddf7ed3c4c40c6b4f9e13a791070ff8e5951
Linh Phi <linhphi9x94@gmail.com>
2016-03-24 18:42:41 +0700

    Revert "Revert "SystemUI:Fix Qs Text""
    This reverts commit e2ec37d8f875d870a2df94ac333bec24325a9d78.

--- packages/SystemUI/src/com/android/systemui/qs/QSTileView.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSTileView.java
@@ -206,7 +206,10 @@ public class QSTileView extends ViewGroup {
             if (labelText != null) {
                 mLabel.setText(labelText);
             }
-            addView(mLabel);	
+            addView(mLabel);
+	     if (mQsColorSwitch) {
+                mLabel.setTextColor(QsTextColor);
+           	 }	
 	
         }
     }
@@ -515,6 +518,9 @@ public class QSTileView extends ViewGroup {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_COLOR_SWITCH),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_ICON_COLOR),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -535,6 +541,11 @@ public class QSTileView extends ViewGroup {
 	if (uri.equals(Settings.System.getUriFor(
                     Settings.System.QS_COLOR_SWITCH))) {
 		 updateColors();
+		setIconColor();
+		}
+	if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_ICON_COLOR))) {
+		updateColors();
 		setIconColor();
 		}
         }
